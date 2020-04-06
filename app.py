4d6d7430ed1a94b3a817ce14f124cf6d81a564be
mrsaicharan1 <saicharan.reddy1@gmail.com>
2020-04-06 21:50:33 +0530

    Authentication Workflow completed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
@@ -11,7 +11,8 @@ from forms import *
 import os
 import bcrypt
 import boto3
-
+from boto3.dynamodb.conditions import Key, Attr
+import traceback
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -53,19 +54,76 @@ def provider_signup():
                     'address': request.form['address'],
                 }
             )
+            # ID generation for wards
+
             print('Signup Successful')
-    return render_template('forms/signup.html')
+    return render_template('forms/provider_signup.html')
 
-
-@app.route('/seeker/login')
-def seeker_login():
-    pass
-
-@app.route('/provider/login')
-def provider_login():
+@app.route('/seeker-signup', methods=['POST','GET'])
+def seeker_signup():
     if request.method == 'POST':
-        pass
+        table = dynamodb.Table('seeker')
+        try:
+            user = table.get_item(
+                Key = {
+                    'email': request.form['email'],
+                }
+            )
+            flash('Account already exists. Please log in')
+        except Exception as e:
+            table = dynamodb.Table('seeker')
+            hashed_password = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt(10))
+            table.put_item(
+            Item={
+                    'name': request.form['name'],
+                    'seeker_id': generate_uuid(),
+                    'password': hashed_password,
+                    'email': request.form['email'],
+                    'address': request.form['address'],
+                }
+            )
+            print('Signup Successful')
+    return render_template('forms/seeker_signup.html')
 
+
+@app.route('/seeker-login', methods=['POST','GET'])
+def seeker_login():
+    table = dynamodb.Table('seeker')
+    if request.method == 'POST':
+        try:
+            response = table.scan(
+                FilterExpression=Attr('email').eq(request.form['email'])
+            )
+            user_record = response['Items'][0]
+            print(user_record)
+            if bcrypt.checkpw(request.form['password'].encode('utf-8'), user_record['password'].value):
+                session['email'] = request.form['email']
+                print("login successful")
+        except Exception as e:
+            return traceback.format_exc()
+
+
+    return render_template('forms/seeker_login.html')
+    
+
+@app.route('/provider-login', methods=['POST', 'GET'])
+def provider_login():
+    table = dynamodb.Table('provider')
+    if request.method == 'POST':
+        try:
+            response = table.scan(
+                FilterExpression=Attr('email').eq(request.form['email'])
+            )
+            user_record = response['Items'][0]
+            print(user_record)
+            if bcrypt.checkpw(request.form['password'].encode('utf-8'), user_record['password'].value):
+                session['email'] = request.form['email']
+                print("login successful")
+        except Exception as e:
+            return traceback.format_exc()
+
+
+    return render_template('forms/provider_login.html')
 
 #----------------------------------------------------------------------------#
 # Launch.
--- generate_dynamodb.py
+++ generate_dynamodb.py
@@ -1,30 +1,63 @@
 import boto3
-# Get the service resource.
-dynamodb = boto3.resource('dynamodb')
 
-# Create the DynamoDB table.
-table = dynamodb.create_table(
-    TableName='providers',
-    KeySchema=[
-        {
-            'AttributeName': 'provider_id',
-            'KeyType': 'HASH'
+def schema_providers_gen():
+    dynamodb = boto3.resource('dynamodb')
+
+    # Create the DynamoDB table.
+    table = dynamodb.create_table(
+        TableName='provider',
+        KeySchema=[
+            {
+                'AttributeName': 'provider_id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'provider_id',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
         }
-    ],
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'provider_id',
-            'AttributeType': 'S'
+    )
+
+    # Wait until the table exists.
+    table.meta.client.get_waiter('table_exists').wait(TableName='provider')
+
+    # Print out some data about the table.
+    print(table.item_count)
+
+def schema_seekers_gen():
+    dynamodb = boto3.resource('dynamodb')
+
+    # Create the DynamoDB table.
+    table = dynamodb.create_table(
+        TableName='seeker',
+        KeySchema=[
+            {
+                'AttributeName': 'seeker_id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'seeker_id',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
         }
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-)
+    )
 
-# Wait until the table exists.
-table.meta.client.get_waiter('table_exists').wait(TableName='providers')
+    # Wait until the table exists.
+    table.meta.client.get_waiter('table_exists').wait(TableName='providers')
 
-# Print out some data about the table.
-print(table.item_count)
+    # Print out some data about the table.
+    print(table.item_count)
+
+schema_seekers_gen()
